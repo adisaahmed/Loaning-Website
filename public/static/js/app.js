94942fcd4980659657f79d036cb071150370ec5e
Ademola Oladipo <styccs@gmail.com>
2016-08-10 23:11:19 +0100

    merging conflicst

---
+++ public/static/js/app.js
@@ -0,0 +1,14 @@
+/**
+ * Created by stikks on 8/7/16.
+ */
+'use strict';
+
+var _app = angular.module('mainApp', []);
+
+_app.controller('LoginController', function () {
+    
+});
+
+_app.controller('HomeController', function () {
+    
+});
